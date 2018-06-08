059a4049908c1175bbe3d71b51dfcfbbfe9ddbdd
Bryan Wyatt <brwyatt@gmail.com>
2018-06-07 22:37:20 -0700

    Add code to create user and download code

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,5 +4,46 @@
 #
 # @example
 #   include catbot::install
-class catbot::install {
+class catbot::install (
+  String $home = '/opt/catbot',
+  String $git_deploy_key,
+  String $git_repo = 'git@github.com:brwyatt/catbot.git',
+){
+  include ::git
+  include ::python
+
+  Exec {
+    path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  }
+
+  user { 'catbot':
+    ensure         => 'present',
+    home           => $home,
+    managehome     => true,
+    password       => '!',
+    purge_ssh_keys => true,
+    system         => true,
+    shell          => '/bin/false',
+  }
+
+  file { "${home}/.ssh":
+    ensure => directory,
+    owner  => 'catbot',
+    mode   => '0775',
+  }
+
+  file { "${home}/.ssh/id_rsa":
+    ensure  => file,
+    owner   => 'catbot',
+    mode    => '0400',
+    content => $git_deploy_key,
+  }
+
+  exec { 'Clone catbot repo':
+    command => "git clone '${git_repo}' catbot",
+    unless  => '[ -d catbot/.git ]',
+    cwd     => $home,
+    user    => 'catbot',
+    require => [Class['git'], File["${home}/.ssh/id_rsa"]],
+  }
 }
